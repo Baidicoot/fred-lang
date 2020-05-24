178f9b4eb7ed8b14b6f54c29d3f92a0d250aaeba
Aidan K. Ewart <aidanprattewart@gmail.com>
2020-05-24 13:07:01 +0100

    added support for multi-module loading

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ To start, run `python client.py`. In the interpreter, simply type the path of th
 ```
 fred> myprogram.fred
 ```
+
+## Modules
+This implementation of Fred adds support for 'modules' - imported programs that add extra functionality to your program. To include a module, simply put `@module-file-path` in your program file.
--- client.py
+++ client.py
@@ -1,21 +1,36 @@
 import eval
 import std
 
-def loadPrg(name):
+def loadPrg(name, loaded):
     scope = {}
+    if name in loaded:
+        return scope
+    loaded.append(name)
     with open(name, "r") as file:
         for l in file.readlines():
             line = l.lstrip().rstrip()
             if line == "":
                 continue
-            s, d = line.split(":")
-            symbol = s.rstrip()
-            defn = d.lstrip().split()
-            scope[symbol] = defn
+            elif line[0] == "@":
+                module = loadPrg(line[1:], loaded)
+                for k in module:
+                    if k in scope:
+                        raise Exception("merge error", k)
+                    scope[k] = module[k]
+            else:
+                s, d = line.split(":")
+                symbol = s.rstrip()
+                defn = d.lstrip().split()
+                scope[symbol] = defn
     return scope
 
 def runFile(name):
-    scope = loadPrg(name)
+    loaded = []
+    scope = loadPrg(name, loaded)
+    print("loaded", len(loaded), "modules:", end=" ")
+    for m in loaded:
+        print(m, end=" ")
+    print("\n")
     state = eval.State(scope["main"], scope, eval.ops)
     while True:
         try:
